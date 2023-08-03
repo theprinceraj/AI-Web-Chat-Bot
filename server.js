01e65f0d59ecb7e52c898ec52a0d00c46436c787
Prince Raj <116755566+theprinceraj@users.noreply.github.com>
2023-08-03 07:31:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.use(express.static(__dirname,'public'));
 
+const openai = new OpenAIApi(new Configuration({
+    // replace your-api-key with your API key from ChatGPT
+    apiKey: 'your-api-key'
+}));
+
 app.listen(3000, () => {
   console.log('Server live at http://localhost:3000');
 });
