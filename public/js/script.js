b197f1697a095d5c06fe0a988d692288f30d664c
Prince Raj <116755566+theprinceraj@users.noreply.github.com>
2023-08-03 07:23:29 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,9 @@ btn.addEventListener('click', getResponse);
 async function getResponse() {                  
   var inputText = document.getElementById("input").value;      
   const parentDiv = document.getElementById("chat-area");
-  
+
+  if(inputText === '') { 
+    return;
+  }
   // The remaining code goes inside this function
 }
