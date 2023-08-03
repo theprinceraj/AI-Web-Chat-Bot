5053bf985817397b859d77ebc2cc35c003a7c060
Prince Raj <116755566+theprinceraj@users.noreply.github.com>
2023-08-03 07:32:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,21 @@ const openai = new OpenAIApi(new Configuration({
     apiKey: 'your-api-key'
 }));
 
+app.post('/chat', async (req, res)=> {   
+  try {
+    const resp = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+        messages: [
+          { role: "user", content: req.body.question}
+        ]  
+    })           
+        
+    res.status(200).json({message: resp.data.choices[0].message.content})
+  } catch(e) {
+      res.status(400).json({message: e.message})
+  }
+})
+
 app.listen(3000, () => {
   console.log('Server live at http://localhost:3000');
 });
